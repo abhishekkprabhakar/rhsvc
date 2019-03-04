ecb97c4d27a85853f37ea634676ad551d3a9978c
abhishekkprabhakar <abhishek@nettoraid.com>
2019-03-04 16:10:58 +0530

    homescreen

---
+++ precache-manifest.0ad3ec2d0adc733de75c5f7194e27da7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "713ac08dfb7141494d4a69f344ff69fd",
+    "url": "/static/media/Nunito-SemiBold.713ac08d.ttf"
+  },
+  {
+    "revision": "1185ede1cdff5c2a8169",
+    "url": "/static/css/main.b7a96a05.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3a24ca0566d4a8661de5",
+    "url": "/static/js/2.3a24ca05.chunk.js"
+  },
+  {
+    "revision": "58e0adc339b27ddf6175ed6c8a5eb44e",
+    "url": "/static/media/sample_action.58e0adc3.jpg"
+  },
+  {
+    "revision": "5bb2efa0f4268eb23ffe69a5fd55c119",
+    "url": "/static/media/action_supporter_icon.5bb2efa0.svg"
+  },
+  {
+    "revision": "65bb0a158ee1967292ee4d11079d45ae",
+    "url": "/static/media/Nunito-Regular.65bb0a15.ttf"
+  },
+  {
+    "revision": "76e62b379c08d7d94c86ad5301d4abb5",
+    "url": "/static/media/Nunito-Black.76e62b37.ttf"
+  },
+  {
+    "revision": "1185ede1cdff5c2a8169",
+    "url": "/static/js/main.1185ede1.chunk.js"
+  },
+  {
+    "revision": "c41c2502180f63ce383b6e3cc042617a",
+    "url": "/static/media/Nunito-Light.c41c2502.ttf"
+  },
+  {
+    "revision": "8f56148bb7b75bdf3358914c28cb798f",
+    "url": "/static/media/Nunito-ExtraBold.8f56148b.ttf"
+  },
+  {
+    "revision": "6f47bcfc065790f02ed3cb8b51bef56f",
+    "url": "/static/media/Nunito-Bold.6f47bcfc.ttf"
+  },
+  {
+    "revision": "26b889a2d7729b23764311dcc10f9a28",
+    "url": "/static/media/Hamburger_menu.26b889a2.svg"
+  },
+  {
+    "revision": "77b73a5681945bbad06e2e5d649ccfa5",
+    "url": "/static/media/homescreen_image_default.77b73a56.svg"
+  },
+  {
+    "revision": "77b73a5681945bbad06e2e5d649ccfa5",
+    "url": "/static/media/image_default.77b73a56.svg"
+  },
+  {
+    "revision": "cfde659baf02739332f0365a8e6799ce",
+    "url": "/index.html"
+  }
+];
